1d4fae8df9096ff810af574ec992f14eb02ec57d
acnasser <acn8701@uncw.edu>
2024-04-19 00:34:17 +0000

    newcommit

--- final_planner.py
+++ final_planner.py
@@ -9,6 +9,7 @@ from ur5e_control.msg import Plan
 from geometry_msgs.msg import Twist
 from robot_vision_lectures.msg import SphereParams
 from geometry_msgs.msg import PointStamped
+from std_msgs.msg import UInt8
 
 points_in_camera = []
 first_point = []
@@ -47,20 +48,24 @@ def main():
 
     while not rospy.is_shutdown():
         if len(points_in_camera) != 0:
+            
             plan = Plan()
-            plan_point = Twist()
-
-           
+            
             plan_point1 = Twist()
+            point_mode1 = UInt8()
+
             plan_point1.linear.x = first_point[0]
-            plan_point1.linear.y = first_point[1]
+            plan_point1.linear.y = first_point[1] 
             plan_point1.linear.z = first_point[2]
             plan_point1.angular.x = 3.09069
             plan_point1.angular.y = 0.06071
             plan_point1.angular.z = 1.56788
+            point_mode1.data = 0
+            
             plan.points.append(plan_point1)
-            print(first_point)
-
+            plan.modes.append(point_mode1)
+            
+			#--------------------------------------------------------------------
            
             pt_in_cam = PointStamped()
             pt_in_cam.header.frame_id = 'camera_color_optical_frame'
@@ -69,34 +74,72 @@ def main():
             pt_in_cam.point.y = points_in_camera[1]
             pt_in_cam.point.z = points_in_camera[2]
             points_in_base = tfBuffer.transform(pt_in_cam, 'base', rospy.Duration(1.0))
-            print(points_in_base)
+   			
+   			#--------------------------------------------------------------------
            
-            plan_point5 = Twist()
-            plan_point5.linear.x = points_in_base.point.x + 0.06
-            plan_point5.linear.y = points_in_base.point.y
-            plan_point5.linear.z = points_in_base.point.z
-            plan_point5.angular.x = 3.09069
-            plan_point5.angular.y = 0.06071
-            plan_point5.angular.z = 1.56788
-            plan.points.append(plan_point5)
-           
+            plan_point2 = Twist()
+            point_mode2 = UInt8()
+            
+            plan_point2.linear.x = points_in_base.point.x + 0.1
+            plan_point2.linear.y = points_in_base.point.y
+            plan_point2.linear.z = points_in_base.point.z
+            plan_point2.angular.x = 3.09069
+            plan_point2.angular.y = 0.06071
+            plan_point2.angular.z = 1.56788
+            point_mode2.data = 1
+            
+            plan.points.append(plan_point2)
+            plan.modes.append(point_mode2)
+            
+            #---------------------------------------------------------------------
+            
             plan_point3 = Twist()
-            plan_point3.linear.x = -0.5
-            plan_point3.linear.y = .3
-            plan_point3.linear.z = 0.4
+            point_mode3 = UInt8()
+            
+            plan_point3.linear.x = points_in_base.point.x + 0.06
+            plan_point3.linear.y = points_in_base.point.y
+            plan_point3.linear.z = points_in_base.point.z
             plan_point3.angular.x = 3.09069
             plan_point3.angular.y = 0.06071
             plan_point3.angular.z = 1.56788
-            plan.points.append(plan_point3)
-   
+            point_mode3.data = 2
+            
+            plan.points.append(plan_point2)
+            plan.modes.append(point_mode3)
+            
+            #--------------------------------------------------------------------
+           
             plan_point4 = Twist()
+            point_mode4 = UInt8()
+            
             plan_point4.linear.x = -0.5
             plan_point4.linear.y = .3
-            plan_point4.linear.z = 0.2
+            plan_point4.linear.z = 0.4
             plan_point4.angular.x = 3.09069
             plan_point4.angular.y = 0.06071
             plan_point4.angular.z = 1.56788
+            point_mode4.data = 2
+            
             plan.points.append(plan_point4)
+            plan.modes.append(point_mode4)
+            
+            #---------------------------------------------------------------------
+   
+            plan_point5 = Twist()
+            point_mode5 = UInt8()
+            
+            plan_point5.linear.x = -0.5
+            plan_point5.linear.y = .3
+            plan_point5.linear.z = 0.2
+            plan_point5.angular.x = 3.09069
+            plan_point5.angular.y = 0.06071
+            plan_point5.angular.z = 1.56788
+            point_mode5.data = 1
+            
+            plan.points.append(plan_point5)
+            plan.modes.append(point_mode5)
+            
+            #---------------------------------------------------------------------
 
             plan_pub.publish(plan)
             loop_rate.sleep()
